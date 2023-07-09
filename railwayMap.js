a4d2ccc064c4f4f9d89d107d0a4307cc47a38667
jae02546 <96040134+jae02546@users.noreply.github.com>
2023-07-09 18:10:59 +0900

    Add files via upload

--- railwayMap.js
+++ railwayMap.js
@@ -220,7 +220,7 @@ async function main() {
           let barX = Math.round(event.touches[0].clientX);
           let barY = Math.round(event.touches[0].clientY);
           if (lastTouchXY[0] != barX || lastTouchXY[1] != barY) {
-            //   lastTouchXY = [barX, barY];
+              lastTouchXY = [barX, barY];
             //   // event.preventDefault(); // デフォルトのブラウザ動作を防ぐ
             //   foo = [
             //     lastPara.translate[0] + lastTouchXY[0] - lastDownXY[0],
