6b82b240d6774033c9fb9aff88497a1b5c8aa718
jae02546 <96040134+jae02546@users.noreply.github.com>
2023-07-09 16:44:16 +0900

    Add files via upload

--- railwayMap.js
+++ railwayMap.js
@@ -238,7 +238,7 @@ async function main() {
           };
           await removeElement(eleSvg);
           await appendSvg(eleSvg, bar, lastGeo);
-          // alert("touchmove2 " + foo);
+          alert("touchmove2 " + foo);
         }
       }
     }
