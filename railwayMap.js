21b6af3379d9fc2bef3ea23703be4d8717b2445c
jae02546 <96040134+jae02546@users.noreply.github.com>
2023-07-12 14:36:21 +0900

    Add files via upload

--- railwayMap.js
+++ railwayMap.js
@@ -225,7 +225,7 @@ async function main() {
         } else if (event.type === "touchmove") {
           console.log(
             funName,
-            "touchmove 12",
+            "touchmove 13",
             event.touches[0].clientX,
             event.touches[0].clientY
           );
